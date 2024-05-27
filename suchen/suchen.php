9acc5e41629ad31857c786e79527708600138075
As-Itachi <danijelritan6@gmail.com>
2024-05-27 10:11:38 +0200

    Versuch erweitete Suche einzufügen

--- suchen/suchen.php
+++ suchen/suchen.php
@@ -1,74 +1,61 @@
+<?php
 
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
-
-    <?php
-
-    if (isset($_POST['submit'])) {
-        if (empty($_POST['job'])) {
-            echo "<h3>Bitte füllen Sie zuerst alle Pflichtfelder aus</h3>";
-        } else {
-            $job = $_POST['job'];
-
-            try{
-                require_once("db.php");
-
-                $sql = $pdo->prepare("SELECT * FROM riskantejobs WHERE bezeichnung LIKE :beschreibung");
-                $job = "%" . $jobs . "%";
-                $sql->bindParam(":beschreibung", "$job");
-                $sql->execute();
-
-            }catch(PDOException $e){
-                echo $e->getMessage();
-            }
-            echo "Test";
-            try{
-                if ($sql->rowCount() > 0) {
-                    while ($row = $sql->fetch()) {
-                        echo "<div class='job-ausgabe'>";
-                        echo "<h3>" . $row['bezeichnung'] . "</h3>";
-                        echo "<p>" . $row['beschreibung'] . "</p>";
-                        echo "</div>";
-                    }
-                } else {
-                    echo "<h3>Keine Jobs gefunden!</h3>";
-                }
-            }catch(PDOException $y){
-                echo $y->getMessage();
-            }
-            
-        }
+if (isset($_POST['submit'])) {
+    if (empty($_POST['job'])) {
+        echo "<h3>Bitte füllen Sie zuerst alle Pflichtfelder aus</h3>";
     } else {
+        $job = $_POST['job'];
 
-    ?>
+        try {
+            require_once("db.php");
 
-        <img src="rj_logo.jpg">
+            $test ="";
 
-        <form>
+            $searchTerms = explode(' ', $job);
 
-            <p><img style="float: left;" src="rj_feuerwehr.jpg" /></p>
+            $sql = $pdo->prepare("SELECT * FROM riskantejobs WHERE :schleife");
 
-            <h2>Riskante Job - Suche</h2>
+            //$sql->bindParam(":beschreibung1", '%' . $searchTerms[0] . '%');
+            for ($i = 0; $i < count($searchTerms); $i++) {
+                $test .= " beschreibung LIKE " . ($searchTerms[$i]) .  " OR ";
+            }
+            echo $test;
+            $sql->bindParam(":schleife", $test);
+            $sql->execute();
 
-            <label for="job">Suchen Sie ihren Traumjob</label>
-            <input type="text" name="job" id="job" required> <br>
-
-            <input type="submit" value="submit" name="submit"> <br>
-
-        </form>
-
-    <?php
+            if ($sql->rowCount() > 0) {
+                while ($row = $sql->fetch()) {
+                    echo "<div class='job-ausgabe'>";
+                    echo "<h3>" . $row['bezeichnung'] . "</h3>";
+                    echo "<p>" . $row['beschreibung'] . "</p>";
+                    echo "</div>";
+                }
+            } else {
+                echo "<h3>Keine Jobs gefunden!</h3>";
+            }
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
     }
-    ?>
+} else {
 
-</body>
+?>
 
-</html>
+    <img src="rj_logo.jpg">
+
+    <form enctype="multipart/form-data" method="POST" action="<?php echo $_SERVER['SCRIPT_NAME']; ?>">
+
+        <p><img style="float: left;" src="rj_feuerwehr.jpg" /></p>
+
+        <h2>Riskante Job - Suche</h2>
+
+        <label for="job">Suchen Sie ihren Traumjob</label>
+        <input type="text" name="job" id="job"> <br>
+
+        <input type="submit" value="submit" name="submit"> <br>
+
+    </form>
+
+<?php
+}
+?>
